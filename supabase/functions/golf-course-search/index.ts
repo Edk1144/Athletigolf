3602a5dcbc661e643f31003fc52bc5cee5e2f5b5
Edk1144 <athletigolf@outlook.com>
2026-07-18 00:16:50 +0100

    Deploy golf course search function

--- supabase/functions/golf-course-search/index.ts
+++ supabase/functions/golf-course-search/index.ts
@@ -37,7 +37,8 @@ type CourseDetail = SearchCourse & {
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Client-Info, Apikey",
 };
 
 const apiBase = "https://api.golfcourseapi.com/v1";
